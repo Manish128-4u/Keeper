ebd25ca526f5c03b387a2177076dbf1c242f6043
Manish128-4u <53977826+Manish128-4u@users.noreply.github.com>
2022-01-12 12:31:59 +0530

    Added Footer

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import react from "react";
 import ReactDOM from "react-dom";
 import Header from "./Header";
 import Note from "./Note";
+import Footer from "./Footer";
+
 ReactDOM.render(
   <div>
     <Header></Header>
     <Note></Note>
+    <Footer></Footer>
   </div>,
 
   document.getElementById("root")
