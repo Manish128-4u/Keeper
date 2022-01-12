b9bae4536c8cc6c99bc5ef121f83207f7f19151a
Manish128-4u <53977826+Manish128-4u@users.noreply.github.com>
2022-01-12 12:28:22 +0530

    Added Note component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import react from "react";
 import ReactDOM from "react-dom";
 import Header from "./Header";
-ReactDOM.render(<Header></Header>, document.getElementById("root"));
+import Note from "./Note";
+ReactDOM.render(
+  <div>
+    <Header></Header>
+    <Note></Note>
+  </div>,
+
+  document.getElementById("root")
+);
